417fabd1e3adf5b412e4c8659bd2d2c07dd6e139
HuaijinX <hjxin@bu.edu>
2023-10-04 15:15:59 -0400

    2

--- strawberry.R
+++ strawberry.R
@@ -1,3 +1,10 @@
 strwb<-read.csv("/Users/xinhuaijin/Desktop/assignments/strawberry/strwb_oct4.csv")
 strwb_census<- strwb[strwb$Program == 'CENSUS',]
 strwb_survey<- strwb[strwb$Program == 'SURVEY',]
+any(is.na(strwb_census))
+head(strwb_census)
+vals<-strwb_census$Value
+vals[1:20]
+vals<-gsub(",","",vals)
+vals<-gsub("(D)",NA,vals)
+vals<-as.numeric(vals)
