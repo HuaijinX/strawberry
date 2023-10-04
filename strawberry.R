a4d68a134fe710097d6165eaf014e070cbea0c54
HuaijinX <hjxin@bu.edu>
2023-10-04 14:51:05 -0400

    1

---
+++ strawberry.R
@@ -0,0 +1,3 @@
+strwb<-read.csv("/Users/xinhuaijin/Desktop/assignments/strawberry/strwb_oct4.csv")
+strwb_census<- strwb[strwb$Program == 'CENSUS',]
+strwb_survey<- strwb[strwb$Program == 'SURVEY',]
